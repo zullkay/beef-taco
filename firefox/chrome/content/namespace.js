03b6cc894fc3980c6d34e8b2f0a3d68fcb024f39
Zullkay II <harissingle41@gmail.com>
2015-08-26 22:57:55 +0800

    Delete namespace.js

--- firefox/chrome/content/namespace.js
+++
@@ -1,5 +0,0 @@
-// Build up the namespace.
-if( ! com ) var com = {};
-if( ! com.github ) com.github = {};
-if( ! com.github.jmhobbs ) com.github.jmhobbs = {};
-if( ! com.github.jmhobbs.beef_taco ) com.github.jmhobbs.beef_taco = {};
